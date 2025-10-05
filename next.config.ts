c8950a838a36885d0c7b62c04c2b060dd4fe127f
Cristian Aragon <crisarag8.sal@gmail.com>
2025-10-05 14:51:54 -0600

    feat: add image remote patterns and specify Node.js engine version in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "foreigners-corner-up-ags.odoo.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
   async redirects() {
     return [
       // Force canonical domain at www.kurisari.dev
